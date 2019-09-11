04ef7f3dd39954292bbff6111771b148c81c495f
Jaymie Xu <jake9828@gmail.com>
2019-09-10 23:32:57 -0400

    Initial commit

---
+++ Assets/Script/ChaseBehaviourController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseBehaviourController : BehaviourController
+{
+    private void Start()
+    {
+    }
+
+    public override void Execute()
+    {
+    }
+}
---
+++ Assets/Script/UserInputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputController : InputController
+{
+    public KeyCode Up, Down, Left, Right;
+
+    private void Start()
+    {
+        Blackboard.Insert<float>("X", 0);
+        Blackboard.Insert<float>("Y", 0);
+    }
+
+    public override void Execute()
+    {
+        Vector2 vel = new Vector2(0, 0);
+
+        if (Input.GetKeyDown(Up))
+        {
+            vel.y = 1;
+        }
+
+        if (Input.GetKeyDown(Down))
+        {
+            vel.y = -1;
+        }
+
+        if (Input.GetKeyDown(Left))
+        {
+            vel.x = -1;
+        }
+
+        if (Input.GetKeyDown(Right))
+        {
+            vel.x = 1;
+        }
+
+        vel.Normalize();
+
+        Blackboard.Insert("X", vel.x);
+        Blackboard.Insert("Y", vel.y);
+    }
+}
